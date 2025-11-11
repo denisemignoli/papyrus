4dbcb1649325a89079b03927a983a497d938e68a
Denise Mignoli <denisemignoli@gmail.com>
2025-11-10 23:31:01 -0300

    feat: add endpoint to add new book

--- src/main/java/com/example/papyrus/Controller.java
+++ src/main/java/com/example/papyrus/Controller.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,5 +57,10 @@ public class Controller {
                 .distinct()
                 .toList();
         return autoresUnicos;
+    }
+
+    @PostMapping("/novoLivro")
+    public boolean cadastraLivro(@RequestBody Livro novoLivro) {
+        return livros.add(novoLivro);
     }
 }
