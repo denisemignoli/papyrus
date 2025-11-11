0f99232ad954b4577744709d34fac6e4a48675d7
Denise Mignoli <denisemignoli@gmail.com>
2025-11-11 00:07:18 -0300

    feat: Add endpoint to filter books by year and improve get by ID response

--- src/main/java/com/example/papyrus/Controller.java
+++ src/main/java/com/example/papyrus/Controller.java
@@ -3,11 +3,13 @@ package com.example.papyrus;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,13 +34,13 @@ public class Controller {
     }
 
     @GetMapping("/{id}")
-    public Livro getLivroByID(@PathVariable String id) {
+    public ResponseEntity<Livro> getLivroByID(@PathVariable String id) {
         for (Livro livro : livros) {
             if (livro.getId().equals(id)) {
-                return livro;
+                return ResponseEntity.ok(livro);
             }
         }
-        return null;
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/titulos")
@@ -62,5 +64,16 @@ public class Controller {
     @PostMapping("/novoLivro")
     public boolean cadastraLivro(@RequestBody Livro novoLivro) {
         return livros.add(novoLivro);
+    }
+
+    @GetMapping("/por-ano")
+    public List<Livro> getLivrosPorAno(@RequestParam int ano) {
+        List<Livro> livrosPorAno = new ArrayList<>();
+        for (Livro livro : livros) {
+            if (livro.getAno() == ano) {
+                livrosPorAno.add(livro);
+            }
+        }
+        return livrosPorAno;
     }
 }
